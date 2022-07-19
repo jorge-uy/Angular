d7257c5d92dbbe140c5f65adfe7c28470d755285
jorge-uy <jdev.uy@gmail.com>
2022-07-19 13:04:33 -0300

    Entrega Desafio N°1_

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './Components/toolbar/toolbar.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { ContentComponent } from './Components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
